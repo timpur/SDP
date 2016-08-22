9e5ba12d45b684c54ea77b0337e5a9bc87e3142d
timpur <timothy@purchas.com>
2016-08-23 00:10:11 +1000

    Updated Libaries

---
+++ SDP/src/WebApp/Scripts/index.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="jquery-3.1.0.intellisense.js" />
+/// <reference path="bootstrap.js" />
+/// <reference path="angular.js" />
+/// <reference path="angular-ui/ui-bootstrap.js" />
+
+
+var app = angular.module("App", []);
+
+
+app.controller("Test", function ($scope) {
+    $scope.name = "This is a Test for Angular";
+});
