3742f4daef243e6a5eedfd54dfe2ccafe969cc73
timpur <timothy@purchas.com>
2016-08-23 13:00:28 +1000

    Added DB + API JS functions

---
+++ SDP/src/WebApp/Scripts/API.js
@@ -0,0 +1,49 @@
+﻿/// <reference path="intellisense/angular.js" />
+/// <reference path="intellisense/bootstrap.js" />
+/// <reference path="intellisense/jquery-3.1.0.intellisense.js" />
+/// <reference path="intellisense/jquery-3.1.0.js" />
+/// <reference path="intellisense/ui-bootstrap.js" />
+
+
+
+var API = new function () {
+    this.baseUrl = "/webapi/api";
+
+    this.callAPIGet = function (url, data, success, error) {
+        this.callAPI(url, data, success, error, "GET");
+    }
+
+    this.callAPIPost = function (url, data, success, error) {
+        this.callAPI(url, data, success, error, "POST");
+    }
+
+    this.callAPI = function (url, data, success, error, method) {
+        jQuery.ajax({
+            dataType: "json",
+            method: method,
+            url: url,
+            data: data,
+            success: success,
+            headers: { "AppKey": "123456" }
+        });
+    }
+
+
+    //Session API
+
+    this.session = { url: this.baseUrl + "/session" };
+    this.session.sessionTypes = function (active, success, error) {
+        var url = this.url + "/sessionTypes/" + JSON.parse(active);
+        API.callAPIGet(url, null, success, error);
+    };
+
+    this.session.booking = { url: this.session.url + "/booking" };
+    this.session.booking.search = function (data, success, error) {
+        var url = this.url + "/search";
+        API.callAPIGet(url, data, success, error);
+    };
+    this.session.booking.search.dataObj = function () {
+        this.StudentId = 0;
+    };
+
+}
--- SDP/src/WebApp/Scripts/index.js
+++ SDP/src/WebApp/Scripts/index.js
@@ -1,7 +1,9 @@
-﻿/// <reference path="jquery-3.1.0.intellisense.js" />
-/// <reference path="bootstrap.js" />
-/// <reference path="angular.js" />
-/// <reference path="angular-ui/ui-bootstrap.js" />
+﻿/// <reference path="intellisense/angular.js" />
+/// <reference path="intellisense/bootstrap.js" />
+/// <reference path="intellisense/jquery-3.1.0.intellisense.js" />
+/// <reference path="intellisense/jquery-3.1.0.js" />
+/// <reference path="intellisense/ui-bootstrap.js" />
+/// <reference path="API.js" />
 
 
 var app = angular.module("App", []);
@@ -9,4 +11,7 @@ var app = angular.module("App", []);
 
 app.controller("Test", function ($scope) {
     $scope.name = "This is a Test for Angular";
+
+
+
 });
